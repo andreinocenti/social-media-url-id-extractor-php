2295e345a6b54d60360af10b00f2075597fcd235
Andre Inocenti <andreinocenti@gmail.com>
2025-07-18 19:47:46 -0300

    feat: SocialMediaUrlIdExtractor

---
+++ src/SocialMediaUrlIdExtractor.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AndreInocenti\SocialMediaUrlIdExtractor;
+
+use AndreInocenti\SocialMediaUrlIdExtractor\Dto\SocialMediaUrlIdExtractorDto;
+use AndreInocenti\SocialMediaUrlIdExtractor\Platforms\FacebookExtractor;
+use AndreInocenti\SocialMediaUrlIdExtractor\Platforms\InstagramExtractor;
+use AndreInocenti\SocialMediaUrlIdExtractor\Platforms\LinkedInExtractor;
+use AndreInocenti\SocialMediaUrlIdExtractor\Platforms\TikTokExtractor;
+use AndreInocenti\SocialMediaUrlIdExtractor\Platforms\XExtractor;
+use AndreInocenti\SocialMediaUrlIdExtractor\Platforms\YouTubeExtractor;
+use AndreInocenti\SocialMediaUrlValidator\Enums\PlatformsCategoriesEnum;
+use AndreInocenti\SocialMediaUrlValidator\Enums\PlatformsEnum;
+use AndreInocenti\SocialMediaUrlValidator\UrlValidator;
+
+class SocialMediaUrlIdExtractor
+{
+    /**
+     * Extracts the ID from a social media URL.
+     *
+     * @param string $url The social media URL.
+     * @return string|null The extracted ID or null if not found.
+     */
+    public function extract(string $url): ?SocialMediaUrlIdExtractorDto
+    {
+        $validator = new UrlValidator();
+        $provider = $validator->detectSocialMedia($url);
+        $provider = PlatformsEnum::tryFrom($provider ?: '');
+
+        $resourceType = $validator->detectSocialMediaCategory($url);
+        $resourceType = PlatformsCategoriesEnum::tryFrom($resourceType ?: '');
+
+        if (!$provider) {
+            throw new \Exception("Provider (Social Media) is not supported ", 1);
+        }
+        if (!$resourceType) {
+            throw new \Exception("Social Media resource type is not supported ", 1);
+        }
+
+
+        $id = match($provider) {
+            PlatformsEnum::INSTAGRAM => (new InstagramExtractor())->extractId($url, $resourceType),
+            PlatformsEnum::FACEBOOK => (new FacebookExtractor())->extractId($url, $resourceType),
+            PlatformsEnum::YOUTUBE => (new YouTubeExtractor())->extractId($url, $resourceType),
+            PlatformsEnum::X => (new XExtractor())->extractId($url, $resourceType),
+            PlatformsEnum::TIKTOK => (new TikTokExtractor())->extractId($url, $resourceType),
+            PlatformsEnum::LINKEDIN => (new LinkedInExtractor())->extractId($url, $resourceType),
+            default => throw new \Exception("Provider (Social Media) is not supported ", 1),
+        };
+
+        return new SocialMediaUrlIdExtractorDto(
+            url: $url,
+            id: $id,
+            provider: $provider->value,
+            resourceType: $resourceType->value
+        );
+    }
+}
---
+++ tests/Unit/SocialMediaUrlIdExtractorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use AndreInocenti\SocialMediaUrlIdExtractor\SocialMediaUrlIdExtractor;
+use AndreInocenti\SocialMediaUrlIdExtractor\Dto\SocialMediaUrlIdExtractorDto;
+use AndreInocenti\SocialMediaUrlValidator\Enums\PlatformsEnum;
+use AndreInocenti\SocialMediaUrlValidator\Enums\PlatformsCategoriesEnum;
+
+beforeEach(function () {
+    $this->service = new SocialMediaUrlIdExtractor();
+});
+
+it('valid cases return correct DTO', function () {
+    $cases = [
+        ['https://www.instagram.com/johndoe/', 'johndoe', PlatformsEnum::INSTAGRAM, PlatformsCategoriesEnum::PROFILE],
+        ['https://facebook.com/profile.php?id=12345', '12345', PlatformsEnum::FACEBOOK, PlatformsCategoriesEnum::PROFILE],
+        ['https://x.com/user/status/67890', '67890', PlatformsEnum::X, PlatformsCategoriesEnum::POST],
+        ['https://linkedin.com/in/test-user', 'test-user', PlatformsEnum::LINKEDIN, PlatformsCategoriesEnum::PROFILE],
+        ['https://www.linkedin.com/in/janedoe/', 'janedoe', PlatformsEnum::LINKEDIN, PlatformsCategoriesEnum::PROFILE],
+        ['https://www.youtube.com/watch?v=VIDEO123', 'VIDEO123', PlatformsEnum::YOUTUBE, PlatformsCategoriesEnum::VIDEO],
+        ['https://youtu.be/VIDEO123', 'VIDEO123', PlatformsEnum::YOUTUBE, PlatformsCategoriesEnum::VIDEO],
+        ['https://tiktok.com/@user/video/1234567890', '1234567890', PlatformsEnum::TIKTOK, PlatformsCategoriesEnum::VIDEO],
+        ['https://vm.tiktok.com/AbCdEf/', 'AbCdEf', PlatformsEnum::TIKTOK, PlatformsCategoriesEnum::VIDEO],
+    ];
+
+    foreach ($cases as [$url, $id, $platform, $resource]) {
+        $dto = $this->service->extract($url);
+        expect($dto)->toBeInstanceOf(SocialMediaUrlIdExtractorDto::class)
+            ->and($dto->id)->toBe($id)
+            ->and($dto->provider)->toBe($platform->value)
+            ->and($dto->resourceType)->toBe($resource->value);
+    }
+});
+
+it('throws on unsupported provider', function () {
+    $this->service->extract('https://unknown.fake/path');
+})->throws(Exception::class, 'Provider (Social Media) is not supported');
+
+it('throws on invalid URL format', function () {
+    $this->service->extract('not a url');
+})->throws(Exception::class);
